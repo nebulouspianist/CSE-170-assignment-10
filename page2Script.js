b6d25513854119c361395ce1b7cc449d6b60f10d
Keye Li <keyeli@Keyes-MacBook-Pro.local>
2023-12-08 17:26:16 -0800

    added files

---
+++ page2Script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('form').onsubmit = function () {
+        alert("Success! You just added a new task. Although it is not actually implemented.");
+    };
+});
